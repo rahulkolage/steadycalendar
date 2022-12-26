f615d05c44125d0879d3286a48340b4f32dfc4dd
Rahul Kolage <Rahul@Apples-MacBook-Pro.local>
2022-12-26 16:21:59 +0530

    json serialization code

--- lib/models/calendar.dart
+++ lib/models/calendar.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+import '../config/constants.dart';
 import 'calendar_color.dart';
+import 'calendar_date.dart';
+import 'calendar_dates_converter.dart';
+import 'date_time_converter.dart';
 
+part 'calendar.g.dart';
+
+@JsonSerializable()
+@CalendarDatesConverter()
+@DateTimeConverter()
 class Calendar {
+  @JsonKey(includeIfNull: false)
   int? id;
 
+  // Supabase database uses '_' id, so we have to use special annoation
+  // when serialize or de-serialize use
+  @JsonKey(name: 'user_id')
   String? userID;
 
   String name;
 
+  @JsonKey(name: 'color', required: true)
   String colorHex;
 
+  @JsonKey(name: 'background_slug', required: true)
   String backgroundSlug; // unique background image
 
+  @JsonKey(name: 'calendar_dates', required: false, includeIfNull: false)
   CalendarDates? dates; // custom type
 
   // color getter
-  CalendarColor? get color => colors.firstWhere((e) => colorHex == e.Hex);
+  CalendarColor? get color => colors.firstWhere((e) => colorHex == e.hex);
 
   Calendar(
       {this.id,
@@ -33,4 +50,14 @@ class Calendar {
         colorHex: colors.first.hex,
         backgroundSlug: 'darts@3x.png');
   }
+
+  // factory helper function for json to string and vice versa conversion
+  // $CalendarFromJson will be generated automatically using json serializer
+  
+  // this will be used when data comes from api as string
+  factory Calendar.fromJson(Map<String, dynamic> json) =>
+      _$CalendarFromJson(json);
+
+  // this will be used convert data to json from string
+  Map<String, dynamic> toJson() => _$CalendarToJson(this);
 }
