ad1eaae329bd51bd04262e793195313afc108cf1
Rahul Kolage <rahulkolage@gmail.com>
2022-12-29 17:15:26 +0530

    integration tests updated

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -48,14 +48,39 @@ void main() {
       await tester.tap(find.byKey(ValueKey("cal-${calRepo.cals.first.id}")));
       await tester.pumpAndSettle();
 
-            // fill out form fields
+      // fill out form fields
       const newName = 'updated';
 
       // update name field
       await tester.tap(find.byKey(const ValueKey(CalEdit.nameKey)));
       await tester.pumpAndSettle();
 
-      
+      // NOTE enterText assumes that the form field is already focused
+      await tester.enterText(
+          find.byKey(const ValueKey(CalEdit.nameKey)), newName);
+      await tester.pumpAndSettle();
+
+      // choose a different background
+      await tester.tap(find.byKey(const ValueKey(CalEdit.bgTileKey)));
+      await tester.pumpAndSettle();
+
+      const newBg = 'adventures-begin-mug@3x.png';
+      await tester.tap(find.byKey(const ValueKey(newBg), skipOffstage: false));
+      await tester.pumpAndSettle();
+
+      // choose a different color
+      await tester.tap(find.byKey(const ValueKey(CalEdit.colorTileKey)));
+      await tester.pumpAndSettle();
+
+      const newColor = '469781';
+      await tester
+          .tap(find.byKey(const ValueKey(newColor), skipOffstage: false));
+      await tester.pumpAndSettle();
+
+      await tester.tap(
+          find.byKey(const ValueKey(CalEdit.saveKey), skipOffstage: false),
+          warnIfMissed: false);
+      await tester.pumpAndSettle();
     });
   });
 }
